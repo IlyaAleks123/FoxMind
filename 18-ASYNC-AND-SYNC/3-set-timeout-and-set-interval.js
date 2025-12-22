43be78803cc6448ba570ec0dc8e573cc0a2889ab
IlyaAleks123 <dz230586aiv>
2025-12-22 12:45:23 +0200

    npm

---
+++ 18-ASYNC-AND-SYNC/3-set-timeout-and-set-interval.js
@@ -0,0 +1,16 @@
+/** ### SET TIMEOUT AND SET INTERVAL ### */
+
+/** setTimeout */
+
+setTimeout(() => {
+    console.log('We can see this in 3 second');
+}, 3000);
+
+setTimeout(() => {
+    console.log('We can see this in 1 second');
+}, 1000);
+
+/** setInterval */
+// setInterval(() => {
+//     console.log('You can see this text every second');
+// }, 1000);
